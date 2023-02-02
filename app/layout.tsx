ba5e11bf83e8934b33e1b880a00af260bc08cf3d
Juned Khatri <juned.khatri31@gmail.com>
2023-02-02 15:48:14 +0530

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsWrapper } from '@/components/analytics'
 import './globals.css'
 
 export default function RootLayout({
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   )
 }
