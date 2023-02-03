ecdd72041fa65bfe2791a27108bedf75f7679c67
Juned Khatri <juned.khatri31@gmail.com>
2023-02-04 00:02:18 +0530

    Update meta description

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Income Tax Calculator</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="A straightforward Indian Income Tax calculator for India" />
+      <meta name="description" content="Compare the old and new tax regimes with ease using our Budget 2023 compliant Income Tax Calculator. Get accurate estimates for both regimes and make informed decisions about your taxes. Stay ahead of the changes and calculate your Income Tax now!" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
